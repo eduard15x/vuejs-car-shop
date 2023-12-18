b868620aa0e15f18aaccf203d5a8e950a410aa51
Precup Eduard <precupeduard99@gmail.com>
2023-12-18 02:00:41 +0200

    Create new component - Home Panel and update Home View

---
+++ frontend/src/components/HomePanel.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="home-panel">
+        <div
+            v-for="(service, index) in services"
+            :key="index"
+            class="home-panel__container"
+        >
+            <p class="service-number">{{ index + 1 }}</p>
+            <h2 class="service-title">{{ service.title }}</h2>
+            <p class="service-description">{{ service.description }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HomePanel',
+    props: {
+        services: {
+            type: Array,
+            required: true
+        }
+    }
+}
+</script>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,6 +1,42 @@
 <template>
-  <div>
-    d
+  <div id="home" class="home">
+    <home-banner :bannerInfo="bannerInfo" />
+    <home-panel :services="services" />
   </div>
 </template>
 
+<script>
+import bannerImg from '@/assets/banner.jpg';
+import HomeBanner from '@/components/HomeBanner.vue';
+import HomePanel from '@/components/HomePanel.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    HomeBanner,
+    HomePanel
+  },
+  data() {
+    return {
+      bannerInfo: {
+        img: bannerImg,
+        imgAlt: 'banner image'
+      },
+      services: [
+        {
+          title: 'Find the right car',
+          description: 'There are cars for everyone.'
+        },
+        {
+          title: 'Get the best offers',
+          description: 'Best offers made by our approved dealers.'
+        },
+        {
+          title: 'Contact our dealers',
+          description: 'Our dealers will respond through our platform, email or phone.'
+        }
+      ]
+    }
+  }
+}
+</script>
